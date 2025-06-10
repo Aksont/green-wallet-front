b19e9ad9f192b1d3524f3b163a0f203fbd78c60f
Aksont <aastanivuk@gmail.com>
2025-06-10 05:27:16 +0200

    .

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,9 @@
 // import type { NextConfig } from "next";
-import { NextConfig } from "next";
 import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  // ✅ place eslint setting here at top level
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // other global settings if needed
-};
-
-// comment
-
 export default withPWA({
-  ...nextConfig,
-  dest: "public", // destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
-  register: true, // register the PWA service worker
-  skipWaiting: true, // skip waiting for service worker activation
+  dest: "public", // ✅ required for next-pwa@5
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
 });
